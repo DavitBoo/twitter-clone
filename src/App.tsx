07d4bbbbc7b26a73aea31d8a1062162cacc5c970
DavitBoo <davidmarras@gmail.com>
2023-05-10 11:35:02 +0200

    main routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { HashRouter , Routes, Route } from "react-router-dom";
+import Home from './Components/Pages/Home';
+import Profile from './Components/Pages/Profile';
+import Settings from './Components/Pages/Settings';
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes >
+      </div>
+    </HashRouter>
   );
 }
 
