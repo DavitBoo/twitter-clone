e32964f5540c52a025a1c3b6b71d048af966b90c
DavitBoo <davidmarras@gmail.com>
2023-05-10 11:27:52 +0200

    global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --color-text: black;
+    --color-background: white;
+    --color-primary: rebeccapurple;
+  }
+`;
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
