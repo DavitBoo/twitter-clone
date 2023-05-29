cf518ca327a4acc2a52b0392ef98a49972c7ec2d
DavitBoo <davidmarras@gmail.com>
2023-05-29 10:23:41 +0200

    cleaning code

--- src/Components/Generic/TopBar.tsx
+++ src/Components/Generic/TopBar.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import SelectMenu from './SelectMenu'
 
+interface TopBarProps {
+  activeMenu: boolean
+  setActiveMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-export default function TopBar({activeMenu, setActiveMenu}: any) {
+export default function TopBar({activeMenu, setActiveMenu}: TopBarProps) {
   return (
     <div>
         <SelectMenu activeMenu={activeMenu} setActiveMenu={setActiveMenu}/>
