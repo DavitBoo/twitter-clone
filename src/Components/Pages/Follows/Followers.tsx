4587a7f859cb10e46aa78d3dd86649205025d737
DavitBoo <davidmarras@gmail.com>
2023-05-30 16:09:14 +0200

    back to profile arrow

--- src/Components/Pages/Follows/Followers.tsx
+++ src/Components/Pages/Follows/Followers.tsx
@@ -1,13 +1,48 @@
 import React, { useContext } from 'react'
+
+// context
+import { UserContext } from '../../../Context/UserContext'
+
+// components
 import UserName from '../../Generic/Subcomponents/UserName'
-import { styled } from 'styled-components'
 import SelectFollowTab from '../../Generic/SelectFollowTab'
 import AccountInfo from '../../Generic/AccountInfo'
-import { UserContext } from '../../../Context/UserContext'
+
+// libraries
+import { styled } from 'styled-components'
+import { NavLink, useParams } from 'react-router-dom'
+
+// firebase
 import { checkUsers } from '../../../firebase/config'
+
+// icons
+import Icon from '@mdi/react';
+import { mdiArrowLeft } from '@mdi/js';
 
 
 const StyledDiv = styled.div `
+    .header{
+      display: flex;
+      align-items: center;
+      padding: 0.5rem 1rem;
+      gap: 2rem;
+
+      a{
+        cursor: pointer;
+        padding: .5rem;
+        border-radius: 100%;
+
+        &:hover{
+          background-color: var(--color-hover);
+        }
+      }
+
+      > div > *{
+        margin: 5px;
+      }
+    }
+
+
     .user-name{
         margin-left: 16px;
 
@@ -28,9 +63,19 @@ export default function Followed() {
 
   // context
   const { userDataState } = useContext(UserContext);
+
+  // use params
+  const { username } = useParams<{ username: string }>();
+  
   return (
     <StyledDiv>
-        <UserName/>
+      
+        <div className='header'>
+          <NavLink to={`/${username}`}>
+                <Icon path={mdiArrowLeft} size={1} />
+          </NavLink>
+          <UserName/>
+        </div>
         <SelectFollowTab/>
         {userDataState?.followers.map((followedUser) => {
         const gotUser = checkUsers(followedUser)
--- src/Components/Pages/Follows/Following.tsx
+++ src/Components/Pages/Follows/Following.tsx
@@ -1,13 +1,47 @@
 import React, { useContext } from 'react'
+
+// use Context
+import { UserContext } from '../../../Context/UserContext'
+
+// components
 import UserName from '../../Generic/Subcomponents/UserName'
-import { styled } from 'styled-components'
 import SelectFollowTab from '../../Generic/SelectFollowTab'
 import AccountInfo from '../../Generic/AccountInfo'
-import { UserContext } from '../../../Context/UserContext'
+
+// libraries
+import { styled } from 'styled-components'
+import { NavLink, useParams } from 'react-router-dom'
+
+// firebase
 import { checkUsers } from '../../../firebase/config'
+
+// icons
+import Icon from '@mdi/react';
+import { mdiCalendarMonthOutline, mdiArrowLeft } from '@mdi/js';
 
 
 const StyledDiv = styled.div `
+    .header{
+      display: flex;
+      align-items: center;
+      padding: 0.5rem 1rem;
+      gap: 2rem;
+
+      a{
+        cursor: pointer;
+        padding: .5rem;
+        border-radius: 100%;
+
+        &:hover{
+          background-color: var(--color-hover);
+        }
+      }
+
+      > div > *{
+        margin: 5px;
+      }
+    }
+
     .user-name{
         margin-left: 16px;
 
@@ -28,9 +62,17 @@ export default function Following() {
   // context
   const { userDataState } = useContext(UserContext);
 
+  // use params
+  const { username } = useParams<{ username: string }>();
+
   return (
     <StyledDiv>
-      <UserName />
+      <div className='header'>
+        <NavLink to={`/${username}`}>
+              <Icon path={mdiArrowLeft} size={1} />
+        </NavLink>
+        <UserName />
+      </div>
       <SelectFollowTab />
       {userDataState?.following.map((followingUser) => {
         const gotUser = checkUsers(followingUser)
