c24779b4116fb5c14aa144289f96aec6219eb697
DavitBoo <davidmarras@gmail.com>
2023-06-20 17:17:57 +0200

    small correction

--- src/Components/Pages/Login.tsx
+++ src/Components/Pages/Login.tsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // libraries
-import { NavLink } from 'react-router-dom'
-import { styled } from 'styled-components'
-import { useNavigate } from 'react-router'
-
+import { NavLink } from "react-router-dom";
+import { styled } from "styled-components";
+import { useNavigate } from "react-router";
 
 // images
-import googleLogo from '../../assets/google-logo.svg'
-import logo from '../../assets/logo.png'
+import googleLogo from "../../assets/google-logo.svg";
+import logo from "../../assets/logo.png";
 
 // firebase
-import { createUserInFirestore, signInWithGoogle } from '../../firebase/config'
+import { createUserInFirestore, signInWithGoogle } from "../../firebase/config";
 
-
-
-const StyledDiv = styled.div `
+const StyledDiv = styled.div`
   z-index: 1000;
   position: relative;
 
-  img{
+  img {
     max-width: 40px;
     filter: drop-shadow(0px 4px 2px hsl(220deg 60% 50%));
   }
 
-  .overlay-login{
+  .overlay-login {
     position: fixed;
-    background: linear-gradient(135deg, var(--color-primary-darker), var(--color-primary));
-    top:0;
+    background: linear-gradient(
+      135deg,
+      var(--color-primary-darker),
+      var(--color-primary)
+    );
+    top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     z-index: -1;
   }
 
-  .container{
+  .container {
     position: fixed;
     top: 50%;
     left: 50%;
@@ -56,7 +57,11 @@ const StyledDiv = styled.div `
   }
 
   h2 {
-    background: linear-gradient(to right, var(--color-primary), var(--color-primary-darker));
+    background: linear-gradient(
+      to right,
+      var(--color-primary),
+      var(--color-primary-darker)
+    );
     background-clip: text;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
@@ -68,56 +73,52 @@ const StyledDiv = styled.div `
     padding: 8px 16px;
     display: flex;
     justify-content: center;
-    gap: .5rem;
+    gap: 0.5rem;
     align-items: center;
 
-    &:hover{
-      background-color:var(--color-primary-70);
+    &:hover {
+      background-color: var(--color-primary-70);
     }
 
-    img{
+    img {
       max-height: 20px;
       background-color: #fff;
       border-radius: 100%;
     }
   }
 
-  a{
+  a {
     color: var(--color-primary);
 
-    &:hover{
+    &:hover {
       text-decoration: underline;
     }
   }
 
-  .error-message{
+  .error-message {
     color: #ff4646;
     margin-top: 10px;
     font-size: 14px;
     text-align: center;
   }
+`;
 
-`
-
-interface LoginProps{
+interface LoginProps {
   setLogged: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-
-
-export default function Login({setLogged}: LoginProps)   {
-  
+export default function Login({ setLogged }: LoginProps) {
   // useState
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   // react-router hooks -  useNavigate
   const navigate = useNavigate();
-  
+
   const handleLogin = async () => {
     try {
-      setLoading(true)
-      setError('')
+      setLoading(true);
+      setError("");
 
       const user = await signInWithGoogle();
       await createUserInFirestore(user);
@@ -127,25 +128,34 @@ export default function Login({setLogged}: LoginProps)   {
       // El usuario ha iniciado sesión correctamente
     } catch (error) {
       console.log(error);
-      setError('An error occurred during login. Please try again.');
+      setError("An error occurred during login. Please try again.");
     } finally {
-      navigate('/');
+      navigate("/");
       setLoading(false); // Establecer loading en false
     }
-  }
+  };
 
   return (
     <StyledDiv>
-        <div className="overlay-login"></div> 
-        <div className="container">
-          <img src={logo} alt="" />
-          <h2>Sign in to WriteLine</h2>
-          <button onClick={handleLogin}>
-            {loading ? 'Loading...' : <><img src={googleLogo} alt="" /> Sign in with Google</>}
-          </button>
-          {error && <p className="error-message">{error}</p>}
-          <p>Don't you have an account? <NavLink to="/">Take a look to the main feed!</NavLink></p>
-        </div>
+      <div className="overlay-login"></div>
+      <div className="container">
+        <img src={logo} alt="" />
+        <h2>Sign in to WriteLine</h2>
+        <button onClick={handleLogin}>
+          {loading ? (
+            "Loading..."
+          ) : (
+            <>
+              <img src={googleLogo} alt="" /> Sign in with Google
+            </>
+          )}
+        </button>
+        {error && <p className="error-message">{error}</p>}
+        <p>
+          Don't you have an account?{" "}
+          <NavLink to="/">Take a look to the main feed!</NavLink>
+        </p>
+      </div>
     </StyledDiv>
-  )
+  );
 }
